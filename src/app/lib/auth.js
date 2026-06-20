67eaf7edad28c215dead916bd76347904b6f659d
Saumel-Das <saumel.spr@gmail.com>
2026-06-20 20:14:25 +0600

    configured environment variable

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -2,13 +2,13 @@ import { betterAuth } from "better-auth";
 
 export const auth = betterAuth({
   //...other options
-  emailAndPassword: { 
-    enabled: true, 
-  }, 
+  emailAndPassword: {
+    enabled: true,
+  },
   socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-        }, 
-    }, 
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
 });

7ce51a47c538ba076b32ee9139e893aa73c708d9
Saumel-Das <saumel.spr@gmail.com>
2026-06-23 09:07:04 +0600

    configured environment

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -1,7 +1,16 @@
+import dns from "node:dns"
+dns.setServers(["8.8.8.8", "1.1.1.1"])
 import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("omniflex");
 
 export const auth = betterAuth({
-  //...other options
+  database: mongodbAdapter(db, {
+    client,
+  }),
   emailAndPassword: {
     enabled: true,
   },

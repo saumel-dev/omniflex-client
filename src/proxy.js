bfa90fa3591aa6742973d739310b113731780553
Saumel-Das <saumel.spr@gmail.com>
2026-06-28 21:57:54 +0600

    added proxy.js

---
+++ src/proxy.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "./app/lib/auth";
+
+export async function proxy(request) {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+
+    if (!session) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+    console.log(session);
+}
+
+export const config = {
+    matcher: [
+        "/class/:path*",
+        "/forum/:path*",
+        "/dashboard/:path*",
+    ],
+};
